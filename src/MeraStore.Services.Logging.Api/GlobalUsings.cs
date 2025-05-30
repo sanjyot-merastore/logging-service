fbb72339eb1d603bb3b8f9e34d2e524091d87386
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2025-05-30 21:35:05 +0530

    MERA-51: (chrore) Update the logging sdk to use the new Core and Exceptions libraries

--- src/MeraStore.Services.Logging.Api/GlobalUsings.cs
+++ src/MeraStore.Services.Logging.Api/GlobalUsings.cs
@@ -6,5 +6,3 @@ global using Newtonsoft.Json;
 global using Newtonsoft.Json.Converters;
 global using Newtonsoft.Json.Serialization;
 global using Serilog;
-global using Constants = MeraStore.Shared.Kernel.Common.Core.Constants;
-global using ServiceProvider = MeraStore.Shared.Kernel.Common.Exceptions.ErrorsCodes.ServiceProvider;
---
+++ test/MeraStore.Service.Logging.Api.Tests/Features/Requests/Create/CreateApiRequestLogHandlerTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using FluentAssertions;
+using MeraStore.Services.Logging.Application.Features.Requests.Create;
+using MeraStore.Services.Logging.Domain.Models;
+using MeraStore.Services.Logging.Domain.Repositories;
+using MeraStore.Shared.Kernel.Exceptions.Core;
+using Moq;
+
+namespace MeraStore.Service.Logging.Api.Tests.Features.Requests.Create;
+
+public class CreateApiRequestLogHandlerTests
+{
+    private readonly Mock<IApiLogRepository> _mockRepo;
+    private readonly CreateApiRequestLogHandler _handler;
+
+    public CreateApiRequestLogHandlerTests()
+    {
+        _mockRepo = new Mock<IApiLogRepository>();
+        _handler = new CreateApiRequestLogHandler(_mockRepo.Object);
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_Should_Create_And_Return_LogEntry()
+    {
+        // Arrange
+        var command = new CreateApiRequestLogCommand("POST", "/api/data", Encoding.UTF8.GetBytes("{ \"test\": true }"),
+            "application/json", Guid.NewGuid().ToString());
+
+        // Act
+        var result = await _handler.ExecuteAsync(command, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.HttpMethod.Should().Be(command.HttpMethod);
+        result.Url.Should().Be(command.Url);
+        result.ContentType.Should().Be(command.ContentType);
+        result.CorrelationId.Should().Be(command.CorrelationId);
+        result.Timestamp.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+
+        _mockRepo.Verify(r => r.AddRequestLogAsync(It.IsAny<ApiRequestLog>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_Should_Throw_LoggingServiceException_On_Failure()
+    {
+        // Arrange
+        var command = new CreateApiRequestLogCommand("POST", "/api/data", Encoding.UTF8.GetBytes(""),
+            "application/json", Guid.NewGuid().ToString());
+
+        _mockRepo.Setup(r => r.AddRequestLogAsync(It.IsAny<ApiRequestLog>(), It.IsAny<CancellationToken>()))
+                 .ThrowsAsync(new Exception("DB write failed", new InvalidOperationException("Inner boom")));
+
+        // Act
+        Func<Task> act = async () => await _handler.ExecuteAsync(command, CancellationToken.None);
+
+        // Assert
+        await act.Should()
+            .ThrowAsync<BaseAppException>()
+            .Where(ex => ex.Message.Contains("DB write failed") && ex.InnerException is InvalidOperationException);
+
+        _mockRepo.Verify(r => r.AddRequestLogAsync(It.IsAny<ApiRequestLog>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
