2be28682ad511c780ae8d634c8ade757f7f72e08
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2025-06-17 01:31:50 +0530

    MSX-57: Fix log Indexing in logging-api

---
+++ src/MeraStore.Services.Logging.Api/Endpoints/SetupLogIndexTemplatesEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using FastEndpoints;
+using MeraStore.Services.Logging.Domain.Interfaces;
+
+namespace MeraStore.Services.Logging.Api.Endpoints;
+
+public class SetupLogIndexTemplatesEndpoint(ILogIndexTemplateService indexTemplateService) : EndpointWithoutRequest<string>
+{
+    public override void Configure()
+    {
+        Post("index");
+        AllowAnonymous();
+        Summary(s =>
+        {
+            s.Summary = "Pushes all Elasticsearch log index templates";
+            s.Description = "Used to configure log index mappings and templates in Elasticsearch.";
+            s.Response(200, "Success");
+            s.Response(500, "Internal Server Error");
+        });
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await indexTemplateService.SetupTemplatesAsync();
+        await SendAsync("Ok", cancellation: ct);
+    }
+}
